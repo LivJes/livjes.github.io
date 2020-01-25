531c6572dc5c13f0118557ec534785e6c3c14ea5
LivJes <41999662+LivJes@users.noreply.github.com>
2020-01-26 00:34:02 +0100

    added more fortunes

--- src/scripts/specialEvents.ts
+++ src/scripts/specialEvents.ts
@@ -11,13 +11,11 @@ function specialEventHandler(index: number) {
 function chess() {
     document.getElementById("debugText").innerText="chess function";
     let win:boolean  = Math.random() < 0.5;
-    if(win) {
-        heroRef.addItem("silverCoin", 1);
-    }
     while(optionButtons.firstChild) {
         optionButtons.removeChild(optionButtons.firstChild);
     }
     if(win) {
+        heroRef.addItem("silverCoin", 1); //assigned to late because of the text refreshing function
         textElement.innerText = "\"Wooooo! Nice job adventurer! You really know how to play chess. Or do you?\" he says as he looks you playfully into the eye. Did he plan this all along?";
     } else {
         textElement.innerText = "\"You win one you lose one. Better luck next time my friend!\"";
@@ -38,6 +36,12 @@ function fortune() {
         "\"Your wife is sleeping with the blacksmith..oh..that's present, sorry, eh...it will rain next week for 13 minutes.\"",
         "\"When you'll have a choice between life and death, chose death.\"",
         "\"Your fifth child will be hideous.\"",
+        "\"In five days you'll be allergic to red bananas. If you didn't know red bananas exist just ignore this prophecy.\"",
+        "\"Next time you are on a crossroads, turn left.\"",
+        "\"You might want to rethink your diet. I see nasty things in your future.\"",
+        "\"Oh my what a body. Just wink at the lady in red robes and you'll be lucky. Say hi from me will you?\"",
+        "\"You'll drop your sword when you'll need it the most.\"",
+        "\"I don't think it is important for you to know what's coming. It will come anyway.\"",
     ];
     while(optionButtons.firstChild) {
         optionButtons.removeChild(optionButtons.firstChild);
